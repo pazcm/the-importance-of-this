2fa6a80ed34473b207343f677e0ba590bed27463
Marta Paz <pazmarta@gmail.com>
2019-01-03 09:30:02 +0000

    highlight all the boxes of the same number class

--- js/script.js
+++ js/script.js
@@ -3,10 +3,19 @@
 // })
 
 /* change color when cursor enters a button */
-$('button').mouseover(function(){
-   $(this).removeClass('makeRed').addClass('makeBlue'); 
-});
+// $('button').mouseover(function(){
+//   $(this).removeClass('makeRed').addClass('makeBlue'); 
+// });
 
-$('button').mouseleave(function(){
-    $(this).removeClass('makeBlue').addClass('makeRed');
+// $('button').mouseleave(function(){
+//     $(this).removeClass('makeBlue').addClass('makeRed');
+// })
+
+/* highlight all the boxes of the same number class */
+
+$(document).ready(function(){
+    $('.box').on("click", function(){
+        var classNames = $(this).attr("class").split(" ");
+        $("." + classNames[1]).css("background-color", "red");
+    })
 })
