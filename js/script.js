8adc2872e678e01ee600b71f2585aa64d83b4623
Marta Paz <pazmarta@gmail.com>
2019-01-03 09:08:35 +0000

    change color when cursor enters a button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-$('p').click(function(){
-    $(this).slideToggle('slow'); /* select the tag to be acted upon */
+// $('p').click(function(){
+//     $(this).slideToggle('slow'); /* select the tag to be acted upon */
+// })
+
+/* change color when cursor enters a button */
+$('button').mouseover(function(){
+   $(this).removeClass('makeRed').addClass('makeBlue'); 
+});
+
+$('button').mouseleave(function(){
+    $(this).removeClass('makeBlue').addClass('makeRed');
 })
